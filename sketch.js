2b4f700a1ceac83ab3d615c6f063ddeb8e5b5b03
FireFlame74 <sandro.figo@gmx.at>
2018-07-21 15:30:55 +0200

    Added sketch.js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let particles = [];
+const particleCount = 150;
+const maxDistance = 50;
+const maxConnections = 3;
+const minSpeed = 0.05;
+const maxSpeed = 0.15;
+
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    background(41, 47, 54);
+}
+
+
+
+function draw() {
+    background(41, 47, 54);
+}
