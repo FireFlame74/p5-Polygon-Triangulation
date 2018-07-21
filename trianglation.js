6b19b4836a67a4afadc202e209e0f3ce304a1fda
Tobias Moldan <tobias.moldan@gmail.com>
2018-07-21 17:45:26 +0200

    pointChainToVertices finished

--- trianglation.js
+++ trianglation.js
@@ -1,10 +1,5 @@
 
 class Vertex {
-    constructor(x, y) {
-        this.Connections = [];
-        this.x = x;
-        this.y = y;
-    }
 
     constructor(vector) {
         this.Connections = [];
@@ -14,11 +9,41 @@ class Vertex {
 }
 
 class Polygon{
-    constructor(){
-        this.Vertices = [];
-    }
+
     constructor(vertices){
+        this.Vertices = undefined;
+        if(vertices == undefined){
+            vertices = [];
+        }
+
         this.Vertices = vertices;
+    } 
+
+    pointChainToVertices(points){
+
+        let oldVertex = new Vertex([points[0].x, points[0].y]);
+        
+
+        for (let index = 1; index < points.length; index++) {
+            const element = points[index];
+
+            let vertex = new Vertex([element.x,element.y]);
+            console.log(vertex);
+            oldVertex.Connections.push(vertex);
+            vertex.Connections.push(oldVertex);
+            if(index==1){
+                this.Vertices.push(oldVertex);
+            }
+            if(index==points.length-1){
+                this.Vertices[0].Connections.push(vertex);
+            }
+            oldVertex = vertex;
+            this.Vertices.push(vertex);
+        }
+
+        this.Vertices[this.Vertices.length-1].Connections.push([points[0].x,points[0].y]);
+        return this;
     }
 
 }
+
