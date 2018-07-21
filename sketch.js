0b30bdfb3c7baaf4fe7dfa2e06c4df9beba4dd25
FireFlame74 <sandro.figo@gmx.at>
2018-07-21 15:49:27 +0200

    Created point array

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+let points = [
+    createVector(1, 1),
+    createVector(1, 0),
+    createVector(0, -1),
+    createVector(-1, 0),
+];
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     background(41, 47, 54);
